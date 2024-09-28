8145c5db46833869ef38d405429958ea4d9299fa
Navid Nikoo <nikoonavid@yahoo.com>
2024-09-28 10:36:15 -0700

    Updated program so now we have a screen where can switch algorithms and input textbox size

---
+++ src/AlgorithmDictionary.py
@@ -0,0 +1,13 @@
+from algorithms import bubble_sort
+from algorithms import merge_sort
+from algorithms import quick_sort
+from algorithms import radix_sort
+from algorithms import linear_search
+
+AlgDict = {
+    'bubble_sort' : bubble_sort,
+    'merge_sort' : merge_sort,
+    'quick_sort' : quick_sort,
+    'radix_sort' : radix_sort,
+    'linear_search' : linear_search
+}
